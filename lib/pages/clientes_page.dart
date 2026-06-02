d4c1a420775c0e8c290ea950061aa9cd0959cace
Ninassc <ninasepulvedacarvalho@gmail.com>
2026-06-02 17:01:08 -0300

    Add search functionality to ClientesPage; implement filtering and enhance input field design

--- lib/pages/clientes_page.dart
+++ lib/pages/clientes_page.dart
@@ -13,6 +13,8 @@ class ClientesPage extends StatefulWidget {
 class _ClientesPageState extends State<ClientesPage> {
   List<Cliente> clientes = [];
 
+  final TextEditingController controllerPesquisa = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -32,6 +34,18 @@ class _ClientesPageState extends State<ClientesPage> {
 
   @override
   Widget build(BuildContext context) {
+    final clientesFiltrados = clientes.where((cliente) {
+      return cliente.nome.toLowerCase().contains(
+            controllerPesquisa.text.toLowerCase(),
+          ) ||
+          cliente.whatsapp.toLowerCase().contains(
+            controllerPesquisa.text.toLowerCase(),
+          ) ||
+          cliente.empresa.toLowerCase().contains(
+            controllerPesquisa.text.toLowerCase(),
+          );
+    }).toList();
+
     return Padding(
       padding: const EdgeInsets.all(20),
 
@@ -48,14 +62,46 @@ class _ClientesPageState extends State<ClientesPage> {
             ),
           ),
 
+          SizedBox(height: 15),
+
+          TextField(
+            controller: controllerPesquisa,
+
+            decoration: InputDecoration(
+              hintText: "Nome, Contato, Empresa...",
+              hintStyle: TextStyle(color: Colors.white70),
+
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(color: const Color(0xFF028FCF)),
+              ),
+
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(color: const Color(0xFF028FCF)),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                  color: const Color.fromARGB(255, 5, 105, 151),
+                ),
+              ),
+            ),
+
+            onChanged: (_) {
+              setState(() {});
+            },
+          ),
+
           SizedBox(height: 20),
 
           Expanded(
             child: ListView.builder(
-              itemCount: clientes.length,
+              itemCount: clientesFiltrados.length,
 
               itemBuilder: (context, index) {
-                final cliente = clientes[index];
+                final cliente = clientesFiltrados[index];
 
                 return CardClientes(
                   cliente: cliente,
